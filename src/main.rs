4327de1d5c9d26fbe3ee78e082c1ff9be7e87a5d
wesley-lewis <lewis.wes.23@gmail.com>
2023-11-19 13:45:07 +0530

    impl and traits

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,65 @@
-use std::io;
+#![allow(dead_code)]
+
+const PI: f64 = 3.14;
 
 fn main() {
-    calculator();
+    let circle = Circle {
+        x: 2f64, 
+        radius: 7f64,
+    };
+
+    let rectangle = Rectangle {
+        length: 5f64,
+        breadth: 7f64,
+    };
+
+    get_area(&circle);
+    get_area(&rectangle);
 }
 
-fn calculator() {
-    println!("Enter first number: ");
-    let mut input1 = String::new();
-    
-    io::stdin().read_line(&mut input1).expect("Failed to read line");
-
-    println!("Enter second number: ");
-    let mut input2 = String::new();
-    io::stdin().read_line(&mut input2).expect("Failed to read line");
-
-    // Conversion of string to int
-    let aint: i32 = input1.trim().parse().ok().expect("Program only expected numbers");
-    let bint: i32 = input2.trim().parse().ok().expect("Program only expected numbers");
-
-    // Math calculations
-    println!("Sum is: {}", aint + bint);
-    println!("Subtraction is: {}", aint - bint);
-    println!("Multiplication is: {}", aint * bint);
-    println!("Division is: {}", aint / bint);
+fn get_area(geom: &dyn Geometry) {
+    println!("{}", geom.area()); 
 }
+
+trait Geometry {
+    fn area(&self) -> f64;
+}
+
+impl Geometry for Circle {
+    fn area(&self) -> f64 {
+        PI * self.radius * self.radius
+    }
+}
+
+impl Geometry for Rectangle {
+    fn area(&self) -> f64 {
+        self.length * self.breadth
+    }
+}
+
+struct Circle {
+    x: f64,
+    radius: f64,
+}
+
+impl Circle {
+    pub fn get_x(&self) -> f64 {
+        self.x
+    }
+}
+
+struct Rectangle {
+    length: f64, 
+    breadth: f64,
+}
+
+impl Rectangle {
+    pub fn get_length(&self) -> f64 {
+        self.length
+    }
+
+    pub fn get_breadth(&self) -> f64 {
+        self.breadth
+    }
+}
+
