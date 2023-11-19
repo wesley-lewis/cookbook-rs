f09bfe05fa24abe8553773a66878c8fc119c8097
wesley-lewis <lewis.wes.23@gmail.com>
2023-11-19 14:52:21 +0530

    channels in rust

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #![allow(dead_code)]
 //-- ##################################
-//-- Task: Passing values to a thread in Rust 
+//-- Task: Channels in Rust 
 //-- Author: Wesley Lewis
 //-- Version: 1.0.0
 //-- Date: 19 March 17
@@ -9,8 +9,32 @@
 
 use std::thread;
 use std::thread::JoinHandle;
+use std::sync::mpsc::{Sender, Receiver};
+use std::sync::mpsc;
 
 fn main() {
+    let threads = 10;
+    let mut thread_holder = vec![];
+    let (tx, rx): (Sender<i32>, Receiver<i32>) = mpsc::channel();
+
+    for thread_no in 0..threads {
+        let thread_tx = tx.clone();
+
+        thread::spawn(move || {
+            thread_tx.send(thread_no).unwrap();
+            println!("thread {} finished", thread_no);
+        });
+    }
+    
+    for _i in 0..threads {
+        thread_holder.push(rx.recv());
+    }
+    
+
+    println!("{:?}", thread_holder);
+}
+
+fn spawning_joining_threads() {
     let mut join_handles: Vec<JoinHandle<i32>> = Vec::new();
 
     for i in 0..10 {
