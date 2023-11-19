83fd451b08d523112d7716c6c5f540c3fff3cd8e
wesley-lewis <lewis.wes.23@gmail.com>
2023-11-19 14:43:20 +0530

    starting and joining 10 threads

--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,56 @@
 #![allow(dead_code)]
+//-- ##################################
+//-- Task: Passing values to a thread in Rust 
+//-- Author: Wesley Lewis
+//-- Version: 1.0.0
+//-- Date: 19 March 17
+//-- #################################
+//
 
-const PI: f64 = 3.14;
+use std::thread;
+use std::thread::JoinHandle;
 
 fn main() {
-    let circle = Circle {
-        x: 2f64, 
-        radius: 7f64,
-    };
+    let mut join_handles: Vec<JoinHandle<i32>> = Vec::new();
 
-    let rectangle = Rectangle {
-        length: 5f64,
-        breadth: 7f64,
-    };
+    for i in 0..10 {
+        let join_handle = thread::spawn(move || {
+            println!("Iteration: {}", i);
+            i
+        });
 
-    get_area(&circle);
-    get_area(&rectangle);
-}
-
-fn get_area(geom: &dyn Geometry) {
-    println!("{}", geom.area()); 
-}
-
-trait Geometry {
-    fn area(&self) -> f64;
-}
-
-impl Geometry for Circle {
-    fn area(&self) -> f64 {
-        PI * self.radius * self.radius
-    }
-}
-
-impl Geometry for Rectangle {
-    fn area(&self) -> f64 {
-        self.length * self.breadth
-    }
-}
-
-struct Circle {
-    x: f64,
-    radius: f64,
-}
-
-impl Circle {
-    pub fn get_x(&self) -> f64 {
-        self.x
-    }
-}
-
-struct Rectangle {
-    length: f64, 
-    breadth: f64,
-}
-
-impl Rectangle {
-    pub fn get_length(&self) -> f64 {
-        self.length
+        join_handles.push(join_handle);
     }
 
-    pub fn get_breadth(&self) -> f64 {
-        self.breadth
+    let jhs = thread::spawn(move || {
+        for jh in join_handles {
+            match jh.join() {
+                Ok(val) => println!("Thread {} finished", val),
+                Err(_) => println!("Error encountered"),
+            }
+        }
+    });
+
+    let _ = jhs.join();
+}
+
+fn code1() {
+    let x = 1; 
+    let handle = thread::spawn(move || {let y = x;});
+    println!("{:?}", handle.join().unwrap());
+
+    thread::spawn(|| {
+        println!("Hello world from first thread, technically second thread");
+    });
+
+    let join_handle = thread::spawn(|| {
+        println!("Hello world from third thread");
+        3
+    });
+
+    match join_handle.join() {
+        Ok(x) => println!("Third thread returned: {}", x),
+        Err(_) => println!("Third thread panicked"),
     }
 }
 
