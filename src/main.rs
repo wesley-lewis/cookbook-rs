327453e400929a0f410d095cbd0c49d21122a4b5
wesley-lewis <lewis.wes.23@gmail.com>
2023-11-21 15:25:10 +0530

    implemented map

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,69 @@
 #![allow(unused_variables)]
 
 //-- ##################################
-//-- Task: Implementing panic
+//-- Task: Implementing map 
 //-- Author: Wesley Lewis
 //-- Version: 1.0.0
 //-- Date: 19 March 17
 //-- #################################
 //
 
-fn main() {
-    compare_stmt_match(Some("Another Book")); 
-    compare_stmt_match(Some("Rust Cookbook")); 
+#[derive(Debug)]
+enum Food { Apple, Carrot, Potato }
 
-    compare_stmt_unwrap(Some("Rust Cookbook"));
-    compare_stmt_unwrap(Some("Another Book"));
+#[derive(Debug)] 
+struct Peeled(Food);
+
+#[derive(Debug)] 
+struct Chopped(Food);
+
+#[derive(Debug)] 
+struct Cooked(Food);
+
+
+fn main() {
+    let apple = Some(Food::Apple);
+    let carrot = Some(Food::Carrot);
+    let potato = None;
+
+    let cooked_apple = cook(chop(peel(apple)));
+    let cooked_carrot = cook(chop(peel(carrot)));
+    let cooked_potato = process(potato);
+
+    eat(cooked_apple);
+    eat(cooked_carrot);
+    eat(cooked_potato);
+}
+
+fn peel(food: Option<Food>) -> Option<Peeled> {
+    match food {
+        Some(food) => Some(Peeled(food)),
+        None => None,
+    }
+}
+
+fn chop(peeled: Option<Peeled>) -> Option<Chopped> {
+    match peeled {
+        Some(Peeled(food)) => Some(Chopped(food)),
+        None => None,
+    }
+}
+
+fn cook(chopped: Option<Chopped>) -> Option<Cooked> {
+    chopped.map(|Chopped(food)| Cooked(food))
+}
+
+fn process(food: Option<Food>) -> Option<Cooked> {
+    food.map(|f| Peeled(f))
+        .map(|Peeled(f)| Chopped(f))
+        .map(|Chopped(f)| Cooked(f))
+}
+
+fn eat(food: Option<Cooked>) {
+    match food {
+        Some(food) => println!("Mmm. I love {:?}", food),
+        None => println!("Oh no! It wasn't edible.")
+    }
 }
 
 fn compare_stmt_match(input: Option<&str>) {
