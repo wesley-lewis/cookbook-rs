5dcc0f98c4c4d11ea058d3afba3dfd5aadfcefca
wesley-lewis <lewis.wes.23@gmail.com>
2023-11-21 20:31:01 +0530

    done with rust cookbook :)

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 #![allow(unused_variables)]
 
 //-- ##################################
-//-- Task: Routing using nickel
+//-- Task: JSON handling in nickel
 //-- Author: Wesley Lewis
 //-- Version: 1.0.0
 //-- Date: 19 March 17
@@ -11,14 +11,13 @@
 #[macro_use] extern crate nickel;
 use nickel::{Nickel, HttpRouter};
 
+
 fn main() {
     let mut server = Nickel::new();
-    server.get("/user/:userid", middleware! {
-        |request| format!("This is user: {:?}", request.param("userid"))
-    });
-    server.get("/bar", middleware!("This is the /bar handler"));
-    server.get("/a/*/d", middleware!("This matches a/b/d and also a/b/c/d"));
-    server.get("/a/**/d", middleware! ("this matches /a/b/d and also /a/b/c/d"));
+    
+
+    server.get("/", middleware! { "hello world" });
+    
 
     let _ = server.listen("127.0.0.1:3000");
 }
