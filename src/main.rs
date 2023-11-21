050ea66b2300e803db9b094bb21b5282d870a1d7
wesley-lewis <lewis.wes.23@gmail.com>
2023-11-21 19:57:41 +0530

    implementing designators

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,38 @@
 #![allow(unused_variables)]
 
 //-- ##################################
-//-- Task: Implementing common macros in Rust
+//-- Task: Implementing designators
 //-- Author: Wesley Lewis
 //-- Version: 1.0.0
 //-- Date: 19 March 17
 //-- #################################
 //
 
-
-fn main() {
-    let v = vec![1,2,3,4,5];
-    println!("Vector: {:?}", v);
-
-    assert!(true);
-    assert_eq!(5, 3 + 2);
+macro_rules! create_function {
+    ($func_name:ident) => (
+        fn $func_name() {
+        println!("You called {:?}()", stringify!($func_name))
+    }
+    )
 }
 
+macro_rules! print_result {
+    ($expression:expr) => (
+    println!("{:?} = {:?}", stringify!($expression),$expression)
+    )
+}
+
+fn main() {
+    foo();
+    bar();
+
+    print_result!(1u32 + 1);
+
+    print_result!({
+        let x = 1u32;
+        x * x + 2 * x - 1
+    });
+}
+
+create_function!(foo);
+create_function!(bar);
